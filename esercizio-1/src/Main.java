800f867c975c2fbddee7ed5c5fef83e22f535935
Antonio <antoniodat93@gmail.com>
2023-09-26 10:04:57 +0200

    primo esercizio

---
+++ esercizio-1/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 9;
+        double z = ((double)x * (double) y) / 2;
+        System.out.print("x =" + x);
+        System.out.println(", y =" + y);
+        System.out.print("l' area del triangolo è = " + z);
+    }
+}
