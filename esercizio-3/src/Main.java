df900508ed5d3f4604690a343a7ba3c6d669e478
Antonio <antoniodat93@gmail.com>
2023-09-26 10:43:37 +0200

    esercizio 3

---
+++ esercizio-3/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        double x = 12.39;
+        double y = 9.11;
+        int z = (int) x * (int) y;
+        System.out.println("il risultato in numeri interi è = " + z);
+    }
+}
