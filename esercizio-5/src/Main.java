ad82a50f2d4a0c2f7bbc384fe67422e16c5d89b8
Antonio <antoniodat93@gmail.com>
2023-09-26 13:12:52 +0200

    esercizio 5

---
+++ esercizio-5/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("la lunghezza della stringa è " + length());
+    }
+
+    public static int length() {
+        String uno = "Mi chiamo Antonio";
+        return uno.length();
+    }
+}
