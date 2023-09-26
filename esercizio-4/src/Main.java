c4f1dfecd061fe6187746b9bfd2bba45fcd68d4c
Antonio <antoniodat93@gmail.com>
2023-09-26 12:48:51 +0200

    esercizio 4

---
+++ esercizio-4/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("somma di x + y = " + somma(5, 8));
+    }
+
+    public static int somma(int x, int y) {
+        int h = x + y;
+        return h;
+    }
+}
