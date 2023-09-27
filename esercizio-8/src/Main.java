637e1560363b265a6694eac224fb487551af4d17
Antonio <antoniodat93@gmail.com>
2023-09-27 10:02:09 +0200

    Âesercizio 8

---
+++ esercizio-8/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 26;
+        int y = 11;
+        System.out.print(incMulti(x,y));
+    }
+    public static double incMulti(double x,int y){
+        double x2 = x + 13.9;
+        int y2 = y + 5;
+        return x2 * y2;
+    }
+
+}
