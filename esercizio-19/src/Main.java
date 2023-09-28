f51935c08dcae9b9310edd98de8d4ed81445603a
Antonio <antoniodat93@gmail.com>
2023-09-28 13:45:31 +0200

    esercizio 19Â

---
+++ esercizio-19/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 7;
+
+        multi(x);
+    }
+    public static void multi(int x){
+
+        for(int i = 1;i<=10;i++){
+            System.out.println(x*i);
+        }
+    }
+}
