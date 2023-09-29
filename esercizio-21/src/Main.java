49e5cb917dbd2e47d92660ce99a5a6becd53aaf1
Antonio <antoniodat93@gmail.com>
2023-09-29 09:18:30 +0200

    esercizio 21

---
+++ esercizio-21/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int inizio = 10;
+        int fine = 0;
+        stop(inizio,fine);
+    }
+    public static void stop(int a,int b){
+
+        for (int z = a ;z >= b; z--){
+            System.out.println(z);
+            if (z == 5){
+                break;
+            }
+        }
+    }
+}
