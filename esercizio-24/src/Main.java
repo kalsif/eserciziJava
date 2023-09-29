e7fafa97ab533e87157c3f7a72019abc1898e089
Antonio <antoniodat93@gmail.com>
2023-09-29 14:55:31 +0200

    esercizio 24

---
+++ esercizio-24/src/Main.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        char[] array = new char[10];
+        occorenza(array);
+    }
+    public static void occorenza(char[] a) {
+        int x = 0;
+        for (int i = 0; i < a.length; i++) {
+            if (i % 2 == 0) {
+                a[i] = 'a';
+            } else {
+                a[i] = '0';
+            }
+        }
+        System.out.println(a);
+
+        for (int i = 0;i < a.length; i++){
+            if(a[i] == 'a'){
+                x = x + 1;
+            }
+        }
+        if (x != 0){
+            System.out.println(x);
+        }else{
+            System.out.println('0');
+        }
+    }
+
+}
+
+
