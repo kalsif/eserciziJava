37d34178845e6fcc9afe2391eb734720265a72ab
Antonio <antoniodat93@gmail.com>
2023-10-19 15:21:52 +0200

    esercizio 38

--- esercizio-38/src/Main.java
+++ esercizio-38/src/Main.java
@@ -2,10 +2,10 @@ public class Main {
     public static void main(String[] args) {
         int inRange = 30;
 
-        System.out.println(numInRange(inRange));
+        System.out.println(numInRange(inRange,0,20));
     }
-    public static boolean numInRange(int x) {
-        if(x>=0 && x <= 20){
+    public static boolean numInRange(int x,int y,int z) {
+        if(x>=y && x <= z){
             return true;
         }else{
             throw new ArithmeticException("THE NUMBER IS NOT IN RANGE");
