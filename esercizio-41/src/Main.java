1be9dbb2d79e85078c064d116bd63fc32031bbc9
Antonio <antoniodat93@gmail.com>
2023-10-20 21:35:15 +0200

    esercizio corretto 41

--- esercizio-41/src/Main.java
+++ esercizio-41/src/Main.java
@@ -2,13 +2,15 @@ import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
-// a causa di Antonio Buonanno io quest oggi per svolgere questo esercizio non ho potuto studiare il resto arrivederci.
-//se ti stai chiedendo se il fatto che il gioco sia infinito è una cosa voluta si lo è!!
+// ti ho riinviato lo stesso esercizio probabilmente sbagliato ancora
+// una volta ma giusto perchè in origine l avevo fatto così.. poi avevo deciso di
+// utilizzare i double e ho scoperto infine che con float e double
+// le divisioni per zero danno questi tipi di problemi
 public class Main {
     public static void main(String[] args) {
         Random r = new Random();
         Scanner sc = new Scanner(System.in);
-        double[] array = new double[10];
+        int[] array = new int[10];
 
         while(true){
             try {
@@ -16,11 +18,11 @@ public class Main {
                 System.out.println("scegli l indice del array e il divisore = ");
                 dividiPerNum(array, sc);
             }catch (ArrayIndexOutOfBoundsException e) {
-                System.out.println("Indice non presente nell' array");
+                System.out.println("Indice non presente nell' array " + e);
             }catch (ArithmeticException e) {
-                System.out.println("Non puoi dividere per 0");
+                System.out.println("Non puoi dividere per 0 " + e);
             }catch (InputMismatchException e){
-                System.out.println("Non puoi usare altri caratteri!!");
+                System.out.println("Non puoi usare altri caratteri!! " + e);
                 sc.next();
             }finally {
                 System.out.println("MOLTO BENE ANCORA");
@@ -28,20 +30,20 @@ public class Main {
         }
     }
 
-    public static double[] riempiArray(double[] array, Random r) {
+    public static int[] riempiArray(int[] array, Random r) {
         for (int i = 0; i < array.length; i++) {
             array[i] = r.nextInt(0, 10);
         }
         return array;
     }
-    public static void dividiPerNum(double[] array, Scanner sc) {
-        double indice= sc.nextDouble();
-        System.out.println("la tua scelta è = " + array[(int) indice]);
-        double num = sc.nextDouble();
-        double divisione = 0;
+    public static void dividiPerNum(int[] array, Scanner sc) {
+        int indice= sc.nextInt();
+        System.out.println("la tua scelta è = " + array[indice]);
+        int num = sc.nextInt();
+        int divisione = 0;
 
         for (int i = 0; i < array.length; i++) {
-            divisione = array[(int) indice] / num;
+            divisione = array[indice] / num;
         }
 
         System.out.println("Il risultato della divisione è = " + divisione);
