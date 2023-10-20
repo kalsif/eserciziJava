3285047b03fdca4313f6a687cc6321b82d384659
Antonio <antoniodat93@gmail.com>
2023-10-20 21:17:19 +0200

    esercizio corretto 42

--- esercizio-42/src/Main.java
+++ esercizio-42/src/Main.java
@@ -1,17 +1,22 @@
 public class Main {
     public static void main(String[] args) {
             Integer numeratore = null;
-            Integer denominatore = null;
-
-            System.out.println(operazione(numeratore,denominatore));
+            Integer denominatore = 5;
+            try {
+                System.out.println(operazione(numeratore,denominatore));
+            }catch (Exception e){
+                System.out.println(e);
+            }
     }
-    public static String operazione(Integer num,Integer denom){
+    public static Integer operazione(Integer num,Integer denom){
         Integer risultato = 0;
-        try {
-            risultato = num*denom;
-        }catch (NullPointerException e){
-            System.out.println("I valori non possono essere nulli");
+        risultato = num*denom;
+        if(num==null){
+            throw new NullPointerException();
         }
-        return "Quindi il risultato è ancora = " + risultato;
+        if(denom==null){
+            throw new NullPointerException();
+        }
+        return risultato;
     }
 }
