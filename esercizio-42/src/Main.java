ba07f5a93288bdaab3594634a697ff9e217a124f
Antonio <antoniodat93@gmail.com>
2023-10-31 17:52:16 +0100

    esercizio corretto

--- esercizio-42/src/Main.java
+++ esercizio-42/src/Main.java
@@ -1,19 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-            Integer numeratore = 5;
+            Integer numeratore = null;
             Integer denominatore = null;
             try {
                 System.out.println(operazione(numeratore,denominatore));
             }catch (NullPointerException e){
-                System.out.println("Il valore è nullo = "+ e.getMessage());
+                System.out.println(e.getMessage());
             }
     }
     public static Integer operazione(Integer num,Integer denom){
-        Integer risultato = 0;
-        risultato = num*denom;
-        if(num==null||denom==null){
-            throw new NullPointerException();
+        if(num==null && denom!=null){
+            throw new NullPointerException("Il num è nullo");
+        } else if (denom==null&&num!=null) {
+            throw new NullPointerException("il denominatore è nullo");
+        } else if (denom==null&&num==null) {
+            throw new NullPointerException("numeratore e denominatore sono null");
+        }else{
+            return num*denom;
         }
-        return risultato;
     }
 }
