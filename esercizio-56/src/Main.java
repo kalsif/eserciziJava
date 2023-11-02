4b4cd730714d5b65e99162397c2ef5e24b5160d3
Antonio <antoniodat93@gmail.com>
2023-11-02 09:44:38 +0100

    esercizio Pair Valeria

--- esercizio-56/src/Main.java
+++ esercizio-56/src/Main.java
@@ -1,30 +1,23 @@
 import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
-        OffsetDateTime data = OffsetDateTime.parse("2023-03-01T13:00:00Z");
+        ZonedDateTime data = OffsetDateTime.parse("2023-03-01T13:00:00Z").atZoneSimilarLocal(ZoneId.of("Europe/Rome"));
 
-        modificaData(data);
-    }
-    public static void modificaData (OffsetDateTime data){
-        String dataIniziale = data.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM));
-        System.out.println(dataIniziale);
+        data = data.plusYears(1);
+        data = data.minusMonths(1);
+        data = data.plusDays(7);
 
-        OffsetDateTime plusOneYear = data.plusYears(1);
-        String plusOneYear1 = plusOneYear.format(DateTimeFormatter.ofPattern("dd MMMM yyyy",Locale.ITALY));
-        System.out.println(plusOneYear1);
+        String newData = data.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM));
 
-        OffsetDateTime minusOneMonth = data.minusMonths(1);
-        String minusOneMonth1 = minusOneMonth.format(DateTimeFormatter.ofPattern("dd MMMM yyyy",Locale.ITALY));
-        System.out.println(minusOneMonth1);
-
-        OffsetDateTime plusSevenDays = data.plusDays(7);
-        String plusSevenDays1 = plusSevenDays.format(DateTimeFormatter.ofPattern("dd MMMM yyyy",Locale.ITALY));
-        System.out.println(plusSevenDays1);
+        System.out.println(newData);
 
 
     }
+
 }
