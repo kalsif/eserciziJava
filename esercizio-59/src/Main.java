5de434da7888fd2fad1f457342c2ce8fd7ef7a8c
Antonio <antoniodat93@gmail.com>
2023-11-03 11:30:16 +0100

    esercizio 59

---
+++ esercizio-59/src/Main.java
@@ -0,0 +1,49 @@
+import java.time.OffsetDateTime;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
+//Riprendi l'esercizio sulle date :
+//
+//        Crea un oggetto OffsetDateTime da questa stringa 2002-03-01T13:00:00Z
+//        Formatta la data ottenuta in FULL, MEDIUM e SHORT
+//        Stampa le varie versioni
+public class Main {
+    public static void main(String[] args) {
+
+        ZonedDateTime data = ZonedDateTime.parse("2002-03-01T13:00:00Z");
+        OffsetDateTime data1 = OffsetDateTime.parse("2002-03-01T13:00:00Z");
+
+
+        printFull(data);
+        printMed(data1);
+        printShort(data1);
+    }
+
+    public static String printFull(ZonedDateTime data){
+        if(data == null){
+            return null;
+        }
+
+        String formData = data.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.FULL));
+        System.out.println(formData);
+        return formData;
+
+    }
+
+    public static String printMed(OffsetDateTime data1){
+        String formData1 = data1.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM));
+
+        System.out.println(formData1);
+        return formData1;
+    }
+
+    public static String printShort(OffsetDateTime data1){
+        String formData2 = data1.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT));
+
+        System.out.println(formData2);
+        return formData2;
+    }
+
+
+}
---
+++ esercizio-59/src/MainTest.java
@@ -0,0 +1,62 @@
+import org.junit.jupiter.api.Test;
+
+import java.time.OffsetDateTime;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    private Main testing = new Main();
+    private ZonedDateTime data = ZonedDateTime.parse("2002-03-01T13:00:00Z");
+    OffsetDateTime data1 = OffsetDateTime.parse("2002-03-01T13:00:00Z");
+
+    @Test
+    void printFull() {
+        String formData = data.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.FULL));
+        String fullFormatt = testing.printFull(data);
+
+        assertEquals(formData,fullFormatt,"Data formattata in Full style");
+    }
+
+    @Test
+    void printFullNull() {
+        String fullFormatt = null;
+
+        assertNull(fullFormatt,"Null");
+    }
+
+
+    @Test
+    void printMed() {
+        String formData1 = data1.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM));
+        String mediumFormatt = testing.printMed(data1);
+
+        assertEquals(formData1,mediumFormatt,"Data formattata in Medium style");
+    }
+    @Test
+    void printMedNull() {
+        String mediumFormatt = null;
+
+        assertNull(mediumFormatt,"Null");
+    }
+
+    @Test
+    void printShort() {
+        String formData2 = data1.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT));
+        String shortFormatt = testing.printShort(data1);
+
+        assertEquals(formData2,shortFormatt,"Data formattata in Short style");
+    }
+
+    @Test
+    void printShortNull() {
+        String shortFormatt = null;
+
+        assertNull(shortFormatt,"Null");
+    }
+
+
+}
