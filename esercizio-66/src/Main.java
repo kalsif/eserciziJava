b4730c9f3302f586c383ebe797b7dd0879785d19
Antonio <antoniodat93@gmail.com>
2023-11-20 11:56:32 +0100

    esercizio 66

--- esercizio-66/src/Main.java
+++ esercizio-66/src/Main.java
@@ -2,22 +2,55 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
-import java.util.Collection;
 
 public class Main {
     public static void main(String[] args) {
-
-        try {
-
+    try {
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/anto_prova_1","developer","Ciaociao123!");
 
             Statement statement = connection.createStatement();
 
-            ResultSet resultSet = statement.executeQuery("select * from students");
+            String createTable = "CREATE TABLE students(" +
+                    "student_id INT NOT NULL AUTO_INCREMENT," +
+                    "last_name varchar(30)" +
+                    "first_name varchar(30)" +
+                    "PRIMARY KEY (student_id)" +
+                    ")";
 
-            while(resultSet.next()){
-                System.out.println(resultSet.getString("last_name"));
-            }
+            statement.executeUpdate(createTable);
+
+            String utente1 = "INSERT INTO students"+
+                    "('last_name',"+
+                    "'first_name')"+
+                    "VALUSE "+
+                    "('Troiano', "+
+                    "'Antonio')";
+
+            String utente2 = "INSERT INTO students"+
+                    "('last_name',"+
+                    "'first_name')"+
+                    "VALUSE "+
+                    "('Rossi', "+
+                    "'Marco')";
+
+            String utente3 = "INSERT INTO students"+
+                    "('last_name',"+
+                    "'first_name')"+
+                    "VALUSE "+
+                    "('Verdi', "+
+                    "'Gennaro')";
+
+            String utente4 = "INSERT INTO students"+
+                    "('last_name',"+
+                    "'first_name')"+
+                    "VALUSE "+
+                    "('Bianchi', "+
+                    "'Paolo')";
+
+            statement.executeUpdate(utente1);
+            statement.executeUpdate(utente2);
+            statement.executeUpdate(utente3);
+            statement.executeUpdate(utente4);
 
         }catch (Exception e){
             e.printStackTrace();
