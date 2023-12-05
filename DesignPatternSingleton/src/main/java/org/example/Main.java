44376190586f7c314a3c2ab3b7415c8cd5b9cb5c
Antonio <antoniodat93@gmail.com>
2023-12-05 09:38:31 +0100

    Pattern Singleton

--- DesignPatternSingleton/src/main/java/org/example/Main.java
+++ DesignPatternSingleton/src/main/java/org/example/Main.java
@@ -1,14 +1,18 @@
 package org.example;
 
+import static org.example.User.getUser;
+
 public class Main {
+
+
     public static void main(String[] args) {
 
-        User u1 = User.getUser();
+        User u1 = getUser();
 
         u1.stampaInfo();
 
 
-        User u2 = User.getUser();
+        User u2 = getUser();
         u2.setNome("Mario");
         u2.setEta(30);
 
